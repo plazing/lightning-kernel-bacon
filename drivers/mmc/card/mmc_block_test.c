716138591c5a5c3ef7c9098b71214cdea052018a
Lee Susman <lsusman@codeaurora.org>
2014-02-07 14:10:38 +0200

    mmc: card: fix mmc_block_test read handler return value
    In case of illegal buffer passed down by user, return -EFAULT instead
    of count.
    
    Signed-off-by: Lee Susman <lsusman@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/mmc/card/mmc_block_test.c
+++ drivers/mmc/card/mmc_block_test.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2013, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2014, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -2236,7 +2236,7 @@ static ssize_t send_write_packing_test_read(struct file *file,
 			       loff_t *offset)
 {
 	if (!access_ok(VERIFY_WRITE, buffer, count))
-		return count;
+		return -EFAULT;
 
 	memset((void *)buffer, 0, count);
 
@@ -2337,7 +2337,7 @@ static ssize_t err_check_test_read(struct file *file,
 			       loff_t *offset)
 {
 	if (!access_ok(VERIFY_WRITE, buffer, count))
-		return count;
+		return -EFAULT;
 
 	memset((void *)buffer, 0, count);
 
@@ -2448,7 +2448,7 @@ static ssize_t send_invalid_packed_test_read(struct file *file,
 			       loff_t *offset)
 {
 	if (!access_ok(VERIFY_WRITE, buffer, count))
-		return count;
+		return -EFAULT;
 
 	memset((void *)buffer, 0, count);
 
@@ -2580,7 +2580,7 @@ static ssize_t write_packing_control_test_read(struct file *file,
 			       loff_t *offset)
 {
 	if (!access_ok(VERIFY_WRITE, buffer, count))
-		return count;
+		return -EFAULT;
 
 	memset((void *)buffer, 0, count);
 
@@ -2665,7 +2665,7 @@ static ssize_t bkops_test_read(struct file *file,
 			       loff_t *offset)
 {
 	if (!access_ok(VERIFY_WRITE, buffer, count))
-		return count;
+		return -EFAULT;
 
 	memset((void *)buffer, 0, count);
 
@@ -2756,7 +2756,7 @@ static ssize_t long_sequential_read_test_read(struct file *file,
 			       loff_t *offset)
 {
 	if (!access_ok(VERIFY_WRITE, buffer, count))
-		return count;
+		return -EFAULT;
 
 	memset((void *)buffer, 0, count);
 
@@ -2919,7 +2919,7 @@ static ssize_t long_sequential_write_test_read(struct file *file,
 			       loff_t *offset)
 {
 	if (!access_ok(VERIFY_WRITE, buffer, count))
-		return count;
+		return -EFAULT;
 
 	memset((void *)buffer, 0, count);
 
@@ -2995,7 +2995,7 @@ static ssize_t new_req_notification_test_read(struct file *file,
 			       loff_t *offset)
 {
 	if (!access_ok(VERIFY_WRITE, buffer, count))
-		return count;
+		return -EFAULT;
 
 	memset((void *)buffer, 0, count);
 
