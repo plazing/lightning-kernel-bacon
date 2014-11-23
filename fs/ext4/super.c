a6d2d55a481ec46696848680fa7896747afc87b6
imoseyon <imoseyon@gmail.com>
2014-11-23 15:38:25 -0800

    Linux 3.10.60
    * fixed up:
    
        pstore: Fix duplicate {console,ftrace}-efi entries
        usb: dwc3: gadget: fix set_halt() bug with pending transfers
    
    Conflicts:
    	fs/pstore/inode.c
    
    Conflicts:
    	fs/ext4/xattr.c

--- fs/ext4/super.c
+++ fs/ext4/super.c
@@ -1635,13 +1635,6 @@ static int parse_options(char *options, struct super_block *sb,
 					"not specified");
 			return 0;
 		}
-	} else {
-		if (sbi->s_jquota_fmt) {
-			ext4_msg(sb, KERN_ERR, "journaled quota format "
-					"specified with no journaling "
-					"enabled");
-			return 0;
-		}
 	}
 #endif
 	if (test_opt(sb, DIOREAD_NOLOCK)) {
@@ -1960,6 +1953,10 @@ static __le16 ext4_group_desc_csum(struct ext4_sb_info *sbi, __u32 block_group,
 	}
 
 	/* old crc16 code */
+	if (!(sbi->s_es->s_feature_ro_compat &
+	      cpu_to_le32(EXT4_FEATURE_RO_COMPAT_GDT_CSUM)))
+		return 0;
+
 	offset = offsetof(struct ext4_group_desc, bg_checksum);
 
 	crc = crc16(~0, sbi->s_es->s_uuid, sizeof(sbi->s_es->s_uuid));
