4e1d0191f64ce2ffb26b1c36a857589680413b96
nikhil18 <nikhilgolechha1@gmail.com>
2016-05-24 17:23:47 +0530

    fix voltage table

--- drivers/cpufreq/qcom-cpufreq.c
+++ drivers/cpufreq/qcom-cpufreq.c
@@ -428,9 +428,9 @@ static struct cpufreq_frequency_table *cpufreq_parse_dt(struct device *dev,
 			GFP_KERNEL);
 
 	if (!dts_freq_table)
-		return ERR_PTR(-ENOMEM);
+		return -ENOMEM;
 
-	*dts_freq_table = *ftbl;
+	*dts_freq_table = *freq_table;;
 
 	for (i = 0; i < nf; i++)
 		dts_freq_table[i].frequency = data[i];
