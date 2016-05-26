1d99685abbf408bc8d654f2819d9f2c965018af0
eray orçunus <erayorcunus@gmail.com>
2016-05-26 17:50:47 +0300

    alarmtimer: Merge fix

--- kernel/time/alarmtimer.c
+++ kernel/time/alarmtimer.c
@@ -25,7 +25,7 @@
 #include <linux/posix-timers.h>
 #include <linux/workqueue.h>
 #include <linux/freezer.h>
-#include "lpm-levels.h"
+//#include "lpm-levels.h"
 
 #define ALARM_DELTA 120
 
@@ -329,6 +329,7 @@ ktime_t alarm_expires_remaining(const struct alarm *alarm)
  * will wake us from suspend.
  */
 #if defined(CONFIG_RTC_DRV_QPNP) && defined(CONFIG_MSM_PM)
+extern void lpm_suspend_wake_time(uint64_t wakeup_time);
 static int alarmtimer_suspend(struct device *dev)
 {
 	struct rtc_time tm;
@@ -730,9 +731,6 @@ static int alarm_timer_create(struct k_itimer *new_timer)
 
 	if (!alarmtimer_get_rtcdev())
 		return -ENOTSUPP;
-
-	if (flags & ~TIMER_ABSTIME)
-		return -EINVAL;
 
 	if (!capable(CAP_WAKE_ALARM))
 		return -EPERM;
