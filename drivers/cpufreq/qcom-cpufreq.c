f74bfd9a0d5673787c24c49f1219643fdc921c98
nikhil18 <nikhilgolechha1@gmail.com>
2016-06-04 10:18:13 -0700

    fix for voltage control

--- drivers/cpufreq/qcom-cpufreq.c
+++ drivers/cpufreq/qcom-cpufreq.c
@@ -427,9 +427,9 @@ static int cpufreq_parse_dt(struct device *dev)
 			GFP_KERNEL);
 
 	if (!dts_freq_table)
-		return -ENOMEM;
+		return ERR_PTR(-ENOMEM);
 
-	*dts_freq_table = *freq_table;;
+	*dts_freq_table = *ftbl;
 
 	for (i = 0; i < nf; i++)
 		dts_freq_table[i].frequency = data[i];
